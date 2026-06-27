ce2a96c107b08a7722c44395a594ae306a2638dd
prabingupta <prabingupta567@gmail.com>
2026-06-27 21:09:57 +0545

    feat: milestone 10 complete - reports view, notifications, error pages, README

--- reports/urls.py
+++ reports/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 
 app_name = 'reports'
 
 urlpatterns = [
+    path('', views.ReportsIndexView.as_view(), name='index'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.utils import timezone
+from datetime import timedelta
+from books.models import Book, BookCopy
+from members.models import Member
+from borrowing.models import BorrowRecord
 
-# Create your views here.
+
+@method_decorator(login_required, name='dispatch')
+class ReportsIndexView(View):
+    template_name = 'reports/index.html'
+
+    def get(self, request):
+        today = timezone.now()
+        last_30 = today - timedelta(days=30)
+
+        total_books = Book.objects.filter(is_deleted=False).count()
+        total_members = Member.objects.filter(is_deleted=False).count()
+        total_borrows = BorrowRecord.objects.count()
+        borrows_this_month = BorrowRecord.objects.filter(borrow_date__gte=last_30).count()
+        returns_this_month = BorrowRecord.objects.filter(
+            status='returned', return_date__gte=last_30
+        ).count()
+        active_borrows = BorrowRecord.objects.filter(status__in=['borrowed', 'renewed']).count()
+        overdue_list = [b for b in BorrowRecord.objects.filter(status__in=['borrowed', 'renewed']) if b.is_overdue]
+        total_fines = sum(b.fine_amount for b in BorrowRecord.objects.all())
+
+        top_books = (
+            BorrowRecord.objects
+            .values('book_copy__book__title')
+            .annotate(borrow_count=__import__('django').db.models.Count('id'))
+            .order_by('-borrow_count')[:5]
+        )
+
+        context = {
+            'total_books': total_books,
+            'total_members': total_members,
+            'total_borrows': total_borrows,
+            'borrows_this_month': borrows_this_month,
+            'returns_this_month': returns_this_month,
+            'active_borrows': active_borrows,
+            'overdue_count': len(overdue_list),
+            'total_fines': total_fines,
+            'top_books': top_books,
+            'page_title': 'Reports',
+        }
+        return render(request, self.template_name, context)
